49de712eafb14fa519e07b012ce73411f0830c5c
148440 <148440@helenparkhurst.net>
2016-02-03 13:21:35 +0100

    Create inlezen.php

---
+++ inlezen.php
@@ -0,0 +1,18 @@
+<?php
+$bestand = 'tekst.txt';
+
+$alleRegels = FILE($bestand);
+
+print_r($alleRegels);
+
+// zoek op strlen(regel) <= 2 dan is het de lege regel
+// $woorden[] = $regel
+
+
+$woorden[] = "hond";
+$woorden[] = "sushi";
+$woorden[] = "zebra";
+$woorden[] = "pizza";
+$woorden[] = "broek";
+
+?>
